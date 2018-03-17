6eddd65a766a42ecfb776df680053c407598b154
lahin31 <muhammad.lahin@gmail.com>
2018-03-17 22:02:03 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let url = 'https://jsonplaceholder.typicode.com/users';
+let names = [];
+fetch(url)
+    .then(blob => blob.json())
+    .then(data => names.push(...data))
+console.log(names);
+checkMatches = (word, names) => {
+    let rejex = new RegExp(word, 'gi');
+    return names.filter(x => {
+        return x.name.match(rejex);
+    })
+};
+displayMatches = (e) => {
+    let matchedArray = checkMatches(e.target.value, names);
+    let html = matchedArray.map(name => {
+        return `
+            <li>
+                <span>${name.name}, ${name.email}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+};
+let searchInput = document.querySelector('.search');
+let suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
